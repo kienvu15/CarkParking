d17ac06a1b387ee96c3e6ff525a35c83f96c1304
kienvu15 <88vutrunkien@gmail.com>
2025-12-22 16:44:30 +0700

    mat

--- Assets/0_Game/Scripts/PlayerManager.cs
+++ Assets/0_Game/Scripts/PlayerManager.cs
@@ -12,13 +12,20 @@ public class PlayerManager : MonoBehaviour
     [Range(0f, 1f)] public float camSpeed;
     [Range(0f, 50f)] public float pathSpeed;
     private Camera mainCam;
-    private float velocity, camVelocity;
+    private float velocity, camVelocityX, camVelocityY;
+
+    private Rigidbody rb;
+    private Collider col;
+    private Renderer ballRender;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         ball = transform;
         mainCam = Camera.main;
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+        ballRender = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -69,6 +76,59 @@ public class PlayerManager : MonoBehaviour
     private void LateUpdate()
     {
         var CameraNewPos = mainCam.transform.position;
-        mainCam.transform.position = new Vector3(Mathf.SmoothDamp(CameraNewPos.x, ball.transform.position.x, ref camVelocity, camSpeed), CameraNewPos.y, CameraNewPos.z);
+        if (rb.isKinematic)
+            mainCam.transform.position = new Vector3(Mathf.SmoothDamp(CameraNewPos.x, ball.transform.position.x, ref camVelocityX, camSpeed),
+                Mathf.SmoothDamp(CameraNewPos.y, ball.transform.position.y + 3f, ref camVelocityY, camSpeed), CameraNewPos.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            gameObject.SetActive(false);
+            MenuManager.Instance.GameState = false;
+        }
+
+        if (other.CompareTag("Red"))
+        {
+            other.gameObject.SetActive(false);
+            ballRender.material = other.GetComponent<Renderer>().material;
+        }
+        else if (other.CompareTag("Blue"))
+        {
+            other.gameObject.SetActive(false);
+            ballRender.material = other.GetComponent<Renderer>().material;
+        }
+        else if (other.CompareTag("Green"))
+        {
+            other.gameObject.SetActive(false);
+            ballRender.material = other.GetComponent<Renderer>().material;
+        }
+        else if (other.CompareTag("Yellow"))
+        {
+            other.gameObject.SetActive(false);
+            ballRender.material = other.GetComponent<Renderer>().material;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("path"))
+        {
+            rb.isKinematic = false;
+            col.isTrigger = false;
+            rb.linearVelocity = new Vector3(0f, 8f, 0f);
+            pathSpeed = pathSpeed * 2;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("path"))
+        {
+            rb.isKinematic = true;
+            col.isTrigger = true;
+            pathSpeed = pathSpeed /2;
+        }
     }
 }
